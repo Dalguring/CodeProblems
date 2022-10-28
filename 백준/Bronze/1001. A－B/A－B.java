1bbe46f8bfd198263162fd08e72413396f6966dc
Dalguring <108657661+Dalgii@users.noreply.github.com>
2022-10-28 11:41:13 +0900

    [Bronze V] Title: A-B, Time: 148 ms, Memory: 15836 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		print();
+	}
+	
+	public static void print() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		br.close();
+		
+		bw.write(A-B+"\n");
+		bw.flush();
+		bw.close();
+		
+	}
+	
+}
