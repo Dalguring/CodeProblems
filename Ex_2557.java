11bd68e90e763dca9bba26c4b432a5868f4a65c9
Dalguring <108657661+Dalgii@users.noreply.github.com>
2022-10-28 11:39:50 +0900

    Delete Ex_2557.java

--- Ex_2557.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        print();
-    }
-    // Git Auto Push Test
-    public static void print() {
-        System.out.println("Hello World!");
-    }
-}
