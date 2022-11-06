fcbcbde35446891535ace48e7cdeb35d90873c0e
Dalguring <108657661+Dalgii@users.noreply.github.com>
2022-11-06 22:44:01 +0900

    [Bronze V] Title: 구구단, Time: 136 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,11 +1,24 @@
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        for(int i = 1; i <=9; i++) {
-            System.out.println(N + " * " + i + " = " + N*i);
-        }
-        
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+		ggd(N);
+		
+	}
+	
+	public static void ggd(int N) {
+		for(int i = 1; i <= 9; i++) {
+			System.out.printf("%d * %d = %d", N, i, N*i);
+			System.out.println();
+		}
+	}
+
 }
