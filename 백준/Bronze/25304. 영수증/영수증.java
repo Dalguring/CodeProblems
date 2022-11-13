6d4c30c9242e9c60e8a27b52d53898512cdc7623
Dalguring <108657661+Dalgii@users.noreply.github.com>
2022-11-13 21:10:28 +0900

    [Bronze V] Title: 영수증, Time: 132 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,20 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int X = sc.nextInt();
-		int N = sc.nextInt();
-		int[] a = new int[N];
-		int[] b = new int[N];
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int X = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 		int total = 0;
-		for(int i=0; i < a.length; i++) {
-			a[i] = sc.nextInt();
-			b[i] = sc.nextInt();
-			total += a[i]*b[i];
+		
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			total += a*b;
 		}
-		if(total == X) System.out.println("Yes");
-		else		   System.out.println("No");
+		br.close();
+		
+		if(X == total) System.out.println("Yes");
+		else System.out.println("No");
+		
 	}
+
 }
