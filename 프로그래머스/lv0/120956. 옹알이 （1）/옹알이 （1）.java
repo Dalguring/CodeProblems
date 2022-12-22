b5085f3f33a6200abe4e086c21f5a37e5aa5bcb4
Daljee <108657661+Dalguring@users.noreply.github.com>
2022-12-22 21:05:26 +0900

    [level 0] Title: 옹알이 (1), Time: 0.08 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int cnt = 0;
+		for(String s : babbling) {
+			cnt += bab(s);
+		}
+		
+        return cnt;
+    }
+    
+    public static int bab(String bab) {
+		
+		if(bab.contains("aya")) {
+			bab = bab.replace("aya", " ").trim();
+		} 
+		if(bab.contains("ye")) {
+			bab = bab.replace("ye", " ").trim();
+		} 
+		if(bab.contains("woo")) {
+			bab = bab.replace("woo", " ").trim();
+		}
+		if(bab.contains("ma")) {
+			bab = bab.replace("ma", " ").trim();
+		}
+		
+		if(bab.trim().length() == 0) return 1;
+		else return 0;
+		
+	}
+}
