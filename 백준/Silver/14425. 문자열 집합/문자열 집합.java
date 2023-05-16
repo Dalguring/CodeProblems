d552c69126fea58007128548fb85f40c8f178d4b
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-05-16 10:21:43 +0900

    [Silver III] Title: 문자열 집합, Time: 412 ms, Memory: 30992 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,22 +11,15 @@ public class Main {
         int setCount = Integer.parseInt(st.nextToken());
         int testCount = Integer.parseInt(st.nextToken());
 
-        List<String> setList = new ArrayList<>();
-        List<String> testList = new ArrayList<>();
-
+        Set setList = new HashSet();
         for (int i = 0; i < setCount; i++) {
             setList.add(br.readLine());
         }
-        for (int i = 0; i < testCount; i++) {
-            testList.add(br.readLine());
-        }
-        br.close();
-        Collections.sort(setList);
-        Collections.sort(testList);
         int count = 0;
         for (int i = 0; i < testCount; i++) {
-            if(setList.contains(testList.get(i))) count++;
+            if(setList.contains(br.readLine())) count++;
         }
+        br.close();
 
         System.out.println(count);
     }
