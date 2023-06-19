d1ffda7fad112feceae2b8345d77397ca37a68e0
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-06-19 11:38:04 +0900

    [unrated] Title: 배열 만들기 4, Time: 9.45 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -2,18 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
-        int i = 0;
-        List<Integer> list = new LinkedList<>();
-        while(i < arr.length) {
-            if(list.isEmpty()) list.add(arr[i]);
-            else if(!list.isEmpty() && list.get(list.size() - 1) < arr[i]) list.add(arr[i]);
-            else if(!list.isEmpty() && list.get(list.size() - 1) >= arr[i]) {
-                list.remove(list.get(list.size() - 1));
-                continue;
-            }
-            i++;
-        }
         
-        return list.stream().mapToInt(e -> e).toArray();
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+
+        while(i < arr.length) {
+            if(stack.isEmpty() || stack.peek() < arr[i]) {
+                stack.push(arr[i]);
+                i++;
+            } else if(stack.peek() >= arr[i]) {
+                stack.pop();
+            }
+        }
+
+        return stack.stream().mapToInt(e -> e).toArray();
     }
 }
