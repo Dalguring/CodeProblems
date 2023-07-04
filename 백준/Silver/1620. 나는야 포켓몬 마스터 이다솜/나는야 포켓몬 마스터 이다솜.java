17600d36dc87d00f9292dc46b92ac9092322c035
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-07-04 11:21:29 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 1924 ms, Memory: 176972 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int encyclopedia = Integer.parseInt(st.nextToken());
+        int toCorrect = Integer.parseInt(st.nextToken());
+        Map<String, Integer> pocketmonMap = new HashMap<>();
+        String[] names = new String[encyclopedia + 1];
+        for (int i = 1; i <= encyclopedia; i++) {
+            String input = br.readLine();
+            pocketmonMap.put(input, i);
+            names[i] = input;
+        }
+
+        for (int i = 0; i < toCorrect; i++) {
+            String input = br.readLine();
+            if (input.matches("^[a-zA-Z]*$")) {
+                bw.write(pocketmonMap.get(input) + "\n");
+                bw.flush();
+            } else {
+                bw.write(names[Integer.parseInt(input)] + "\n");
+                bw.flush();
+            }
+        }
+        br.close();
+        bw.close();
+    }
+}
+
