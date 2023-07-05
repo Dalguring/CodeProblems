10dca32ad18be3f41fab2242f32bec53fce18dd2
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-07-05 16:08:50 +0900

    [unrated] Title: 문자열 잘라서 정렬하기, Time: 26.33 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String myString) {
+        StringTokenizer st = new StringTokenizer(myString, "x");
+        List<String> list = new ArrayList<>();
+        
+        while(st.hasMoreTokens())
+            list.add(st.nextToken());
+
+        Collections.sort(list);
+        
+        return list.stream().toArray(String[]::new);
+    }
+}
