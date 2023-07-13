173004c25252477fa542ad68d5be05feeb5a89b7
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-07-13 16:58:02 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 527.09 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,34 +1,32 @@
 class Solution {
     public int[][] solution(int n) {
         int control = 1;
-        int controlX = n - 1;
-        int controlY = n - 1;
+        int controlSpot = n - 1;
         int cycle = 0;
         int[][] answer = new int[n][n];
 
         while(control <= n * n) {
-            for(int i = cycle; i <= controlY; i++) {
+            for(int i = cycle; i <= controlSpot; i++) {
                 answer[cycle][i] = control;
                 control++;
             }
             control--;
-            for(int i = cycle; i <= controlX; i++) {
-                answer[i][controlX] = control;
+            for(int i = cycle; i <= controlSpot; i++) {
+                answer[i][controlSpot] = control;
                 control++;
             }
             control--;
-            for (int i = controlY; i >= cycle; i--) {
-                answer[controlY][i] = control;
+            for (int i = controlSpot; i >= cycle; i--) {
+                answer[controlSpot][i] = control;
                 control++;
             }
             control--;
             cycle++;
-            for (int i = controlX; i >= cycle; i--) {
+            for (int i = controlSpot; i >= cycle; i--) {
                 answer[i][cycle - 1] = control;
                 control++;
             }
-            controlX--;
-            controlY--;
+            controlSpot--;
         }
         
         return answer;
