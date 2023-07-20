093ffde9d8a936c710baa5a15a67dc954c1ad679
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-07-20 15:43:25 +0900

    [level 2] Title: 기능개발, Time: 2.91 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] chargeTo100 = new int[progresses.length];
+        for (int i = 0; i < progresses.length; i++) {
+            chargeTo100[i] = (100 - progresses[i]) % speeds[i] == 0 ? 
+                             (100 - progresses[i]) / speeds[i] : (100 - progresses[i]) / speeds[i] + 1;
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i : chargeTo100)
+            queue.offer(i);
+
+        int target = chargeTo100[0];
+        int cnt = 0;
+        List<Integer> list = new ArrayList<>();
+        while(queue.size() != 0) {
+            if(queue.peek() <= target) {
+                cnt++;
+                queue.poll();
+                if(queue.size() == 0)
+                    list.add(cnt);
+            } else {
+                list.add(cnt);
+                target = queue.peek();
+                cnt = 0;
+            }
+        }
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
