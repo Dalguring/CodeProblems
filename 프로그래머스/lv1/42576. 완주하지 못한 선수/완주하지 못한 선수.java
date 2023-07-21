8518548da7fcc516f28f8a29bd522b9d95060bef
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-07-21 13:40:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.96 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,20 +3,14 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> map = new HashMap<>();
-        
         for(String whomParticipate : participant)
             map.put(whomParticipate, map.getOrDefault(whomParticipate, 0) + 1);
-        
         for(String whomComplete : completion) {
-            if(map.containsKey(whomComplete))
-                map.put(whomComplete, map.get(whomComplete) - 1);
-        }
-        StringBuilder sb = new StringBuilder();
-        for(Map.Entry<String, Integer> el : map.entrySet()) {
-            if(el.getValue() > 0) 
-                sb.append(el.getKey());    
+            map.put(whomComplete, map.get(whomComplete) - 1);
+            if(map.get(whomComplete) == 0)
+                map.remove(whomComplete);
         }
         
-        return sb.toString();
+        return map.keySet().iterator().next();
     }
 }
