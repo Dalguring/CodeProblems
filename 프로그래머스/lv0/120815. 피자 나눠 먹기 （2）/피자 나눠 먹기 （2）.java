dfe7030d393f67e46d03d0970a3144e972142f65
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-07-21 11:20:06 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -2,21 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int n) {
-        int pizzas = 1;
-        
-        return getPizzaSet(n, pizzas);
+
+        return n * 6 / GCD(n, 6) / 6;
     }
     
-    static int getPizzaSet(int n, int pizzas) {
-        while(pizzas * 6 < n) {
-            pizzas++;
-            if(pizzas * 6 >= n) break;
-        }
-        if(pizzas * 6 % n == 0)
-            return pizzas;
-
-        pizzas++;
-
-        return getPizzaSet(n, pizzas);
+    static int GCD(int n, int num) {
+        if(n % num == 0)
+            return num;
+        return GCD(num, n % num);
     }
 }
