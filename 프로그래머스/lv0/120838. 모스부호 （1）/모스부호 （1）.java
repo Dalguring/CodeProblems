7ca835460d34ef903e237933075a559f14a6a92f
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-07-25 16:09:25 +0900

    [level 0] Title: 모스부호 (1), Time: 0.77 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String letter) {
+        String morse = "'.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f','--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l','--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r','...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x','-.--':'y','--..':'z'";
+        String[] morses = morse.replace("'", "").replace(",", " ").split(" ");
+        Map<String, String> map = new HashMap<>();
+        for(String s : morses)
+            map.put(s.split(":")[0], s.split(":")[1]);
+        System.out.println(Arrays.toString(morses));
+        System.out.println(map);
+        StringBuilder sb = new StringBuilder();
+        for(String s : letter.split(" "))
+            sb.append(map.get(s));
+        
+        return sb.toString();
+    }
+}
