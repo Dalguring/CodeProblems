5da15f63fb5c6e32f267faf5577e281ae6445d5b
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-07-25 09:39:50 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 120 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int in = Integer.parseInt(br.readLine());
+        br.close();
+        System.out.println(in);
+        System.out.println("1");
+    }
+}
