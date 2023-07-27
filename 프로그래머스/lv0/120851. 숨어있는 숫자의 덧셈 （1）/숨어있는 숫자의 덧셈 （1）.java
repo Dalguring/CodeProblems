fd0403e475ea3445495e578fd24e7086767954dc
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-07-27 11:10:36 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.19 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,10 +1,9 @@
 class Solution {
     public int solution(String my_string) {
+        my_string = my_string.replaceAll("[A-Z|a-z]", "");
         int answer = 0;
-        for(int i = 0; i < my_string.length(); i++) {
-            if(my_string.charAt(i) >= 'A' && my_string.charAt(i) <= 'z') continue;
-            answer += my_string.charAt(i) - '0';
-        }
+        for(char c : my_string.toCharArray()) 
+            answer += c - '0';
         
         return answer;
     }
