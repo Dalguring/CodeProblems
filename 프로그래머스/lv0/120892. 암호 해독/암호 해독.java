e2232809ec05c4b4cafa675b27f0938d684f5e1e
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-07-28 10:37:06 +0900

    [level 0] Title: 암호 해독, Time: 0.05 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -1,10 +1,9 @@
 class Solution {
     public String solution(String cipher, int code) {
-        String answer = "";
-        for(int i = code-1; i < cipher.length(); i+=code) {
-            answer += cipher.charAt(i);
-        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = code - 1; i < cipher.length(); i += code)
+            sb.append(cipher.charAt(i));
         
-        return answer;
+        return sb.toString();
     }
 }
