be65d89742dddfa3cbc1e3dfb1a00bba8133a41c
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-08-01 17:39:14 +0900

    [level 0] Title: 배열의 유사도, Time: 1.38 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
@@ -1,13 +1,13 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 class Solution {
     public int solution(String[] s1, String[] s2) {
         int answer = 0;
-        for(String str2 : s2) {
-            for(String str1 : s1) {
-                if(str2.equals(str1)) {
-                    answer++;
-                    break;
-                }
-            }
+        List<String> list = Arrays.stream(s2).collect(Collectors.toList());
+        for(String s : s1) {
+            if(list.contains(s))
+                answer++;
         }
         
         return answer;
