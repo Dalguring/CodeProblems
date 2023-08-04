8cd7186082877dc62828410779f38ac37fd3d1d9
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-08-04 16:15:04 +0900

    [level 0] Title: 안전지대, Time: 0.14 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.java
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.java
@@ -8,13 +8,10 @@ class Solution {
                 board[a[0]][a[1]] = 1;
         }
         
-        print(board);
-
         int zeroCnt = 0;
         for(int[] a : board) {
-            for(int b : a) {
+            for(int b : a) 
                 if(b == 0) zeroCnt++;
-            }
         }
         
         return zeroCnt;
@@ -24,31 +21,18 @@ class Solution {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 if (board[i][j] == 1) {
-                    try {
-                        set.add(new int[]{i - 1, j - 1});
-                        set.add(new int[]{i - 1, j});
-                        set.add(new int[]{i - 1, j + 1});
-                        set.add(new int[]{i, j - 1});
-                        set.add(new int[]{i, j + 1});
-                        set.add(new int[]{i + 1, j - 1});
-                        set.add(new int[]{i + 1, j});
-                        set.add(new int[]{i + 1, j + 1});
-                    } catch (Exception e) {
-                        continue;
-                    }
+                    set.add(new int[]{i - 1, j - 1});
+                    set.add(new int[]{i - 1, j});
+                    set.add(new int[]{i - 1, j + 1});
+                    set.add(new int[]{i, j - 1});
+                    set.add(new int[]{i, j + 1});
+                    set.add(new int[]{i + 1, j - 1});
+                    set.add(new int[]{i + 1, j});
+                    set.add(new int[]{i + 1, j + 1});
                 }
             }
         }
 
         return set;
-    }
-
-    static void print(int[][] board) {
-        for(int[] a : board) {
-            for(int b : a) {
-                System.out.print(b + " ");
-            }
-            System.out.println();
-        }
     }
 }
