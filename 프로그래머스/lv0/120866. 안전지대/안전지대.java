7cf3e8b1d4bbdaf8c803cd56846b8cec568ae53d
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-08-04 16:00:53 +0900

    [level 0] Title: 안전지대, Time: 9.04 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] board) {
+        Set<int[]> set = new HashSet<>();
+        for(int[] a : returnSet(board, set)) {
+            try {
+                board[a[0]][a[1]] = 1;
+            } catch (Exception e) {
+                continue;
+            }
+        }
+        
+        print(board);
+
+        int zeroCnt = 0;
+        for(int[] a : board) {
+            for(int b : a) {
+                if(b == 0) zeroCnt++;
+            }
+        }
+        
+        return zeroCnt;
+    }
+    
+    static Set<int[]> returnSet(int[][] board, Set<int[]> set) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 1) {
+                    try {
+                        set.add(new int[]{i - 1, j - 1});
+                        set.add(new int[]{i - 1, j});
+                        set.add(new int[]{i - 1, j + 1});
+                        set.add(new int[]{i, j - 1});
+                        set.add(new int[]{i, j + 1});
+                        set.add(new int[]{i + 1, j - 1});
+                        set.add(new int[]{i + 1, j});
+                        set.add(new int[]{i + 1, j + 1});
+                    } catch (Exception e) {
+                        continue;
+                    }
+                }
+            }
+        }
+
+        return set;
+    }
+
+    static void print(int[][] board) {
+        for(int[] a : board) {
+            for(int b : a) {
+                System.out.print(b + " ");
+            }
+            System.out.println();
+        }
+    }
+}
