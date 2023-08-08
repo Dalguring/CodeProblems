4786fb102efb6177bcce4b163f10b38b847eb925
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-08-08 10:46:40 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.09 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] lines) {
+        Map<Integer, Integer> dict = new HashMap<>();
+
+        for (int i = 0; i < lines.length; i++) {
+            int min = Math.min(lines[i][0], lines[i][1]);
+            int max = Math.max(lines[i][0], lines[i][1]);
+
+            for (int j = min; j < max; j++) {
+                dict.put(j, dict.getOrDefault(j, 0) + 1);
+            }
+        }
+
+        int answer = 0;
+
+        for (Map.Entry<Integer, Integer> entry : dict.entrySet()) {
+            if (entry.getValue() >= 2) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
