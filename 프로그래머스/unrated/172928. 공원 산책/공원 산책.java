739c080a4a82485b6b6a6a5af6a4a98b77b6cb60
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-08-16 17:00:34 +0900

    [unrated] Title: 공원 산책, Time: 10.90 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int[] solution(String[] park, String[] routes) {
+        int[] start = new int[2];
+        for(int i = 0; i < park.length; i++) {
+            if(park[i].contains("S")) {
+                start[0] = i;
+                start[1] = park[i].indexOf("S");
+                break;
+            }
+        }
+
+        for(int i = 0; i < routes.length; i++) {
+            int move = Integer.parseInt(routes[i].split(" ")[1]);
+            switch (routes[i].split(" ")[0]) {
+                case "N":
+                    String nStr = "";
+                    if(start[0] - move >= 0) {
+                        for (int j = start[0] - 1; j >= start[0] - move; j--)
+                            nStr += park[j].charAt(start[1]);
+                    } else break;
+
+                    if(nStr.contains("X")) {
+                        nStr = "";
+                        break;
+                    }
+                    else start[0] -= move;
+                    nStr = "";
+                    break;
+                case "S":
+                    String sStr = "";
+                    if(start[0] + move < park.length) {
+                        for (int j = start[0] + 1; j <= start[0] + move; j++)
+                            sStr += park[j].charAt(start[1]);
+                    } else continue;
+
+                    if(sStr.contains("X")) {
+                        sStr = "";
+                        continue;
+                    }
+                    else start[0] += move;
+                    sStr = "";
+                    break;
+                case "W":
+                    if(start[1] - move < 0 || park[start[0]].substring(start[1] - move, start[1]).contains("X"))
+                        continue;
+                    else start[1] -= move;
+                    break;
+                case "E":
+                    if(start[1] + move >= park[start[0]].length() || park[start[0]].substring(start[1] + 1, start[1] + move + 1).contains("X"))
+                        continue;
+                    else start[1] += move;
+                    break;
+            }
+        }
+        
+        return start;
+    }
+}
