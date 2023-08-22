bcc39b20b5c0621e554c0f3cb6b48fd40a7ceff0
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-08-22 13:15:20 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 876 ms, Memory: 59184 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        List<int[]> list = new ArrayList<>();
+        for (int i = 0; i < testCase; i++) {
+            st = new StringTokenizer(br.readLine());
+            list.add(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+        }
+        br.close();
+        list.sort(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] == o2[1])
+                    return o1[0] - o2[0];
+                else
+                    return o1[1] - o2[1];
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(int[] i : list) {
+            sb.append(i[0] + " " + i[1] + "\n");
+        }
+        System.out.println(sb);
+    }
+}
