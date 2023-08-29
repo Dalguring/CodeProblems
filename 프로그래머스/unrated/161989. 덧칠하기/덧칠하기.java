4ceb705c889deeef86aa1c23f27075b88b1891b3
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-08-29 11:28:58 +0900

    [unrated] Title: 덧칠하기, Time: 0.95 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -2,29 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int paint = 0;
-        int[] wall = new int[n];
-        Arrays.fill(wall, 1);
-        for (int i = 0; i < section.length; i++)
-            wall[section[i] - 1] = 0;
-
-        while (true) {
-            paint++;
-            int index = 0;
-            for (int i = 0; i < wall.length; i++) {
-                if (wall[i] == 0) {
-                    index = i;
-                    break;
-                }
+        int roller = section[0];
+        int cnt = 1;
+        for(int i = 1; i < section.length; i++) {
+            if(roller + m - 1 < section[i]) {
+                cnt++;
+                roller = section[i];
             }
-            Arrays.fill(wall, index, Math.min(index + m, wall.length), 1);
-            int sum = 0;
-            for (int i : wall)
-                sum += i;
-            if(sum == wall.length)
-                break;
         }
         
-        return paint;
+        return cnt;
     }
 }
