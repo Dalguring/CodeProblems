f1a4b91d79624c6e07a26cfb23b6bb975723f311
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-08-30 14:01:32 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.26 ms, Memory: 68.2 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,21 +1,19 @@
 class Solution {
     public int solution(int number, int limit, int power) {
-        int answer = 0;
-
+        int[] count = new int[number + 1];    
         for (int i = 1; i <= number; i++) {
-            int divisor = 0;
-            for (int j = 1; j <= Math.sqrt(i); j++) {
-                if (i % j == 0)
-                    divisor += 2;
-                if (i / (double)j == j)
-                    divisor -= 1;
+            for (int j = 1; j <= number / i; j++) {
+                count[i * j]++;
             }
-            if (divisor > limit)
-                answer += power;
-            else
-                answer += divisor;
         }
-        
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            if (count[i] > limit) {
+                answer += power;
+            } else {
+                answer += count[i];
+            }
+        }
         return answer;
     }
 }
