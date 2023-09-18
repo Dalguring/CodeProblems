e9e52a0071d89a497655ccf2738ae968c31d86b6
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-09-18 15:57:27 +0900

    [Bronze I] Title: 단어 공부, Time: 384 ms, Memory: 38700 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,28 +1,31 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<Character, Integer> map = new HashMap<>();
+        String target = br.readLine().toUpperCase();
+        br.close();
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		str = str.toUpperCase();
-		char[] ch = str.toCharArray();
-		sc.close();
-		int[] arr = new int[26];
-		for(int i = 0; i < ch.length; i++) {
-			int index = ch[i]-65;
-			arr[index] += 1;
-		}
-		
-		int max = 0, index = 0, count = 0;
-		for(int i = 0; i < arr.length; i++) { max = Math.max(max, arr[i]); }
-		for(int i = 0; i < arr.length; i++) {
-			if(max == arr[i]) {
-				index = i;
-				count++;
-			}
-		}
-		if(count > 1) {System.out.println("?");}
-        else {System.out.println((char)(index+65));}
-	}
+        for (char c : target.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+        List<Character> keySet = new ArrayList<>(map.keySet());
+
+        keySet.sort(new Comparator<Character>() {
+            @Override
+            public int compare(Character o1, Character o2) {
+                return map.get(o2) - map.get(o1);
+            }
+        });
+
+        char answer;
+
+        if (map.size() > 1)
+            answer = map.get(keySet.get(0)).equals(map.get(keySet.get(1))) ? '?' : keySet.get(0);
+        else answer = keySet.get(0);
+
+        System.out.println(answer);
+    }
 }
