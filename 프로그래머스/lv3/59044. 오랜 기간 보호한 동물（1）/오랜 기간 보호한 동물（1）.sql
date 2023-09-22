df52aaaf734dce32880d4843a74772a26d38d9e6
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-09-22 22:59:59 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,8 +1,9 @@
 SELECT NAME, DATETIME
   FROM ANIMAL_INS
- WHERE ANIMAL_ID NOT IN (SELECT O.ANIMAL_ID
-                           FROM ANIMAL_OUTS O
-                           JOIN ANIMAL_INS I
-                             ON O.ANIMAL_ID = I.ANIMAL_ID)                           
+ WHERE ANIMAL_ID 
+   NOT IN (SELECT O.ANIMAL_ID
+             FROM ANIMAL_OUTS O
+             JOIN ANIMAL_INS I
+               ON O.ANIMAL_ID = I.ANIMAL_ID)                           
  ORDER BY DATETIME
  LIMIT 3;
