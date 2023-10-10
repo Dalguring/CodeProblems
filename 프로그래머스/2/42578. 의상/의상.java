df13fc713ea0c5c0b1513e3e02d2a942377fe25a
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-10-10 17:01:23 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -6,16 +6,11 @@ class Solution {
         for (String[] s : clothes)
             map.put(s[1], map.getOrDefault(s[1], 0) + 1);
 
-        List<Integer> list = new ArrayList<>();
-        for (Map.Entry<String, Integer> el : map.entrySet())
-            list.add(el.getValue() + 1);
-
+        Iterator<Integer> iterator = map.values().iterator();
         int answer = 1;
-        for (int i : list)
-            answer *= i;
-
-        answer--;
+        while (iterator.hasNext())
+            answer *= iterator.next() + 1;
         
-        return answer;
+        return answer - 1;
     }
 }
