1a5c048faad10f139a8a4030cd3ed2cf7d8dbae4
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-10-20 11:21:10 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,24 +1,15 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[]arr = new int[10];
-        int count = 1;
-        for(int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt()%42;
-        }
-        for(int i = 0; i < arr.length-1; i++) {
-        	int index = arr[i];
-        	for(int j=i+1; j < arr.length; j++) {
-        		if(index == arr[j]) {
-        			count--;
-        			break;
-        		}
-        		
-        	}
-        	count++;
-        }
-        System.out.println(count);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < 10; i++)
+            set.add(Integer.parseInt(br.readLine()) % 42);
+        
+        br.close();
+        System.out.println(set.size());
     }
 }
