d048f3614be9fca8f02e81c1944d2da1d7113d1e
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-10-23 13:41:01 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.77 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -3,33 +3,26 @@ import java.util.*;
 
 class Solution {
     public String solution(String X, String Y) {
-        Map<Character, Integer> xMap = new TreeMap<>();
-        Map<Character, Integer> yMap = new TreeMap<>();
+        PriorityQueue<Integer> xpq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> ypq = new PriorityQueue<>(Collections.reverseOrder());
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < X.length(); i++)
-            xMap.put(X.charAt(i), xMap.getOrDefault(X.charAt(i), 0) + 1);
+            xpq.add(X.charAt(i) - '0');
 
         for (int i = 0; i < Y.length(); i++)
-            yMap.put(Y.charAt(i), yMap.getOrDefault(Y.charAt(i), 0) + 1);
+            ypq.add(Y.charAt(i) - '0');
 
-        Iterator<Character> iterator = xMap.keySet().iterator();
-
-        while (iterator.hasNext()) {
-            char c = iterator.next();
-            if (yMap.containsKey(c)) {
-                int min = Math.min(xMap.get(c), yMap.get(c));
-                sb.append(String.valueOf(c).repeat(min));
-            }
-        }
-        if (sb.length() == 0) {
-            sb.append("-1");
-            return sb.toString();
+        while (!xpq.isEmpty() && !ypq.isEmpty()) {
+            if (xpq.peek() == ypq.peek()) {
+                sb.append(xpq.poll());
+                ypq.poll();
+            } else if (xpq.peek() > ypq.peek()) {
+                xpq.poll();
+            } else
+                ypq.poll();
         }
         
-        if (sb.toString().replace("0", "").equals(""))
-            return "0";
-        
-        return sb.reverse().toString();
+        return sb.toString().equals("") ? "-1" : sb.toString().charAt(0) == '0' ? "0" : sb.toString();
     }
 }
