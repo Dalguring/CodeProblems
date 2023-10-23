ae46f304f03838caa5e93efef84c2101468f3b5a
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-10-23 10:30:18 +0900

    [Bronze II] Title: 상수, Time: 124 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,21 +1,17 @@
+import java.io.*;
 import java.util.*;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String[] A = sc.next().split("");
-		String[] B = sc.next().split("");
-		int a = 0; 
-		int b = 0; 
-		for(int i = A.length-1; i >= 0; i--) {
-			a += Integer.parseInt(A[i])*(Math.pow(10, i));
-		}
-		
-		for(int i = B.length-1; i >= 0; i--) {
-			b += Integer.parseInt(B[i])*(Math.pow(10, i));
-		}
-		
-		System.out.println(a > b ? a : b);
-	}
+        StringBuilder sb = new StringBuilder(st.nextToken());
+        int A = Integer.parseInt(sb.reverse().toString());
+        sb.delete(0, sb.length());
+        int B = Integer.parseInt(sb.append(st.nextToken()).reverse().toString());
 
+        System.out.println(Math.max(A, B));
+    }
 }
