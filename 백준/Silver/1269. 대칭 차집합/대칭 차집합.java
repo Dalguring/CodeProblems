d1735fbfe4889dab05c1bffa7d71a5ef9b59e3ad
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-10-26 14:16:36 +0900

    [Silver IV] Title: 대칭 차집합, Time: 712 ms, Memory: 81552 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -4,24 +4,30 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        br.readLine();
-        Set<Integer> hashSet = new HashSet<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        while(st.hasMoreTokens())
-            hashSet.add(Integer.parseInt(st.nextToken()));
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        Map<Integer, Integer> map = new HashMap<>();
         st = new StringTokenizer(br.readLine());
-        br.close();
-        while(st.hasMoreTokens()) {
-            int target = Integer.parseInt(st.nextToken());
-            if( hashSet.contains(target) ){
-                hashSet.remove(target);
-            } else {
-                hashSet.add(target);
-            }
+
+        for (int i = 0; i < A; i++) {
+            map.put(Integer.parseInt(st.nextToken()), 1);
         }
 
-        System.out.println(hashSet.size());
+        st = new StringTokenizer(br.readLine());
 
+        for (int i = 0; i < B; i++) {
+            int target = Integer.parseInt(st.nextToken());
+
+            if (map.containsKey(target))
+                map.remove(target);
+            else
+                map.put(target, 1);
+        }
+        
+        br.close();
+        System.out.println(map.keySet().size());
     }
 }
-
