81c0300bf76042facc14ed4f7592a017962576a6
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-11-08 15:36:26 +0900

    [Bronze V] Title: 사분면 고르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,13 @@
+xAxis = int(input())
+yAxis = int(input())
+
+if xAxis > 0:
+    if yAxis > 0:
+        print(1)
+    else:
+        print(4)
+else:
+    if yAxis > 0:
+        print(2)
+    else:
+        print(3)
