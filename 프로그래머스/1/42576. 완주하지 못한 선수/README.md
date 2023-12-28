0a332b17c42143f970d023079c5c2dc7fa52866a
Daljee <108657661+Dalguring@users.noreply.github.com>
2023-12-28 17:00:38 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 60.53 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 33.9 MB, 시간: 54.03 ms
+메모리: 33.9 MB, 시간: 60.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 4일 16:52:29
+2023년 12월 4일 17:0:36
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,16 +1,12 @@
 def solution(participant, completion):
     playerDict = dict()
-    answer = ''
 
     for player in participant:
-        if player in playerDict:
-            playerDict[player] += 1
-        else:
-            playerDict[player] = 1
+        playerDict[player] = playerDict.get(player, 0) + 1
 
     for player in completion:
         playerDict[player] -= 1
         if playerDict.get(player) == 0:
-            playerDict.pop(player)
+            del playerDict[player]
 
     return playerDict.__iter__().__next__()
