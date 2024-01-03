66d084ee157daf5d55c75c26bd516b6e0b22b8cf
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-01-03 13:31:06 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 368 ms, Memory: 24712 KB -BaekjoonHub

---
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        boolean[] primes = new boolean[10000001];
+        for (int i = 2; i <= Math.sqrt(primes.length); i++) {
+            for (int j = i * i; j < primes.length; j += i) {
+                primes[j] = true;
+            }
+        }
+
+        for (int i = 0; i < testCase; i++) {
+            int count = 0;
+            int target = Integer.parseInt(br.readLine());
+            for (int j = 2; j <= target / 2; j++) {
+                if (!primes[j] && !primes[target - j])
+                    count++;
+            }
+            sb.append(count).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+
+}
