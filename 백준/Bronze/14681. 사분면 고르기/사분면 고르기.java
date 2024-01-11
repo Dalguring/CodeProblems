9b7e1a58cba683c3d5a452e21f37512881660b28
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-01-11 11:19:40 +0900

    [Bronze V] Title: 사분면 고르기, Time: 120 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -1,12 +1,19 @@
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        int k = x > 0 && y > 0 ? 1 :
-                x < 0 && y > 0 ? 2 :
-                x < 0 && y < 0 ? 3 : 4;
-        System.out.println(k);
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean xAxis = Integer.parseInt(br.readLine()) > 0;
+        boolean yAxis = Integer.parseInt(br.readLine()) > 0;
+        boolean[] quadrant = new boolean[]{xAxis, yAxis};
+
+        if (quadrant[0]) {
+            if (quadrant[1]) System.out.println(1);
+            else             System.out.println(4);
+        } else {
+            if (quadrant[1]) System.out.println(2);
+            else             System.out.println(3);
+        }
+        br.close();
     }
 }
