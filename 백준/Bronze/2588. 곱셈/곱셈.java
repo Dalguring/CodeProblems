9e476889742ac74867afa3f48d9e867da4f26389
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-01-11 11:02:08 +0900

    [Bronze III] Title: 곱셈, Time: 120 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,12 +1,20 @@
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        System.out.println(A*(B%10));
-        System.out.println(A*((B%100)-(B%10))/10);
-        System.out.println(A*((B-(B%100)))/100);
-        System.out.println(A*B);
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int first = Integer.parseInt(br.readLine());
+        String second = br.readLine();
+        char[] secondCharArray = second.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 2; i >= 0; i--) {
+            sb.append(first * Character.getNumericValue(secondCharArray[i])).append("\n");
+        }
+
+        sb.append(first * Integer.parseInt(second));
+        System.out.println(sb);
+        
+        br.close();
     }
 }
