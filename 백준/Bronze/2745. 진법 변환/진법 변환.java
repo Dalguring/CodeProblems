74c5456e29a93d1664e1685aa7925ae669e66d31
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-01-17 11:28:58 +0900

    [Bronze II] Title: 진법 변환, Time: 132 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -1,28 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        br.close();
-        String N = st.nextToken();
-        StringBuilder sb = new StringBuilder();
-        for(int i = N.length() - 1; i >= 0; i--) {
-            sb.append(N.charAt(i));
+        Map<Character, Integer> binaryMap = new HashMap<>();
+
+        for (int i = 10; i <= 35; i++) {
+            binaryMap.put((char)('A' - 10 + i), i);
         }
-        int B = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder(st.nextToken()).reverse();
+        int rootNum = Integer.parseInt(st.nextToken());
         int total = 0;
-        for(int i = 0; i < sb.length(); i++) {
-            if(sb.charAt(i) > '9') {
-                int temp = sb.charAt(i) - 'A' + 10;
-                total += temp * Math.pow(B, i);
+
+        for (int i = 0; i < sb.length(); i++) {
+            char target = sb.charAt(i);
+            if (binaryMap.containsKey(target)) {
+                total += binaryMap.get(target) * Math.pow(rootNum, i);
             } else {
-                total += (sb.charAt(i) - '0') * Math.pow(B, i);
+                total += Character.getNumericValue(target) * Math.pow(rootNum, i);
             }
         }
+
         System.out.println(total);
+        br.close();
     }
 }
