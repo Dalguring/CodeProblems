ef163b3545e434e1fc0248d1b6e59aa93afb2638
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-02-22 10:37:08 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,15 +1,18 @@
-SELECT A.HISTORY_ID
-     , B.DAILY_FEE * (A.END_DATE - A.START_DATE + 1)
-       * NVL(1 - DISCOUNT_RATE / 100, 1) AS FEE
-  FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY A
-  JOIN CAR_RENTAL_COMPANY_CAR B
+SELECT HISTORY_ID
+      , A.DAILY_FEE * (END_DATE - START_DATE + 1) 
+                    * (1 - NVL((SELECT DISCOUNT_RATE
+                                  FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+                                 WHERE DURATION_TYPE = CASE WHEN END_DATE - START_DATE + 1 >= 90
+                                                            THEN '90일 이상'
+                                                            WHEN END_DATE - START_DATE + 1 >= 30
+                                                            THEN '30일 이상'
+                                                            WHEN END_DATE - START_DATE + 1 >= 7
+                                                            THEN '7일 이상'
+                                                            ELSE NULL
+                                                             END
+                                   AND CAR_TYPE = '트럭'), 0) / 100) AS FEE
+  FROM CAR_RENTAL_COMPANY_CAR A
+  JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY B
     ON A.CAR_ID = B.CAR_ID
-  LEFT JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN C
-    ON B.CAR_TYPE = C.CAR_TYPE
-   AND C.DURATION_TYPE = (CASE WHEN (A.END_DATE-A.START_DATE+1) >= '90' THEN '90일 이상'
-                               WHEN (A.END_DATE-A.START_DATE+1) >= '30' THEN '30일 이상'
-                               WHEN (A.END_DATE-A.START_DATE+1) >= '7' THEN '7일 이상'
-                               ELSE NULL END)
- WHERE 1=1
-   AND B.CAR_TYPE = '트럭'
- ORDER BY FEE DESC, A.HISTORY_ID DESC;
+ WHERE A.CAR_TYPE = '트럭'
+ ORDER BY FEE DESC, HISTORY_ID DESC;
