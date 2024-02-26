d26c9e15db128fdace79233ab3e78038b0fd25f2
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-02-26 10:36:49 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.41 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,36 +1,26 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
+        String [] answer = new String[n];
+        int[] bitMap = new int[n];
 
         for (int i = 0; i < n; i++) {
-            int length1 = Integer.toBinaryString(arr1[i]).length();
-            int length2 = Integer.toBinaryString(arr2[i]).length();
-            String string1 = "";
-            String string2 = "";
-
-            if (n > length1) {
-                string1 = "0".repeat(n - length1).concat(Integer.toBinaryString(arr1[i]));
-            } else {
-                string1 = Integer.toBinaryString(arr1[i]);
-            }
-            if (n > length2) {
-                string2 = "0".repeat(n - length2).concat(Integer.toBinaryString(arr2[i]));
-            } else {
-                string2 = Integer.toBinaryString(arr2[i]);
-            }
-
-            StringBuilder sb = new StringBuilder();
-
-            for (int j = 0; j < n; j++) {
-                if (string1.charAt(j) != '0' || string2.charAt(j) != '0') {
-                    sb.append("#");
-                } else {
-                    sb.append(" ");
-                }
-            }
-            answer[i] = sb.toString();
+            bitMap[i] = arr1[i] | arr2[i];
+            answer[i] = fillMap(bitMap[i], n);
         }
         
         return answer;
+    }
+    
+    static String fillMap(int target, int size) {
+        int tLength = Integer.toBinaryString(target).length();
+        String binaryString = "";
+
+        if (size > tLength) {
+             binaryString = "0".repeat(size - tLength).concat(Integer.toBinaryString(target));
+        } else {
+            binaryString = Integer.toBinaryString(target);
+        }
+
+        return binaryString.replaceAll("1", "#").replaceAll("0", " ");
     }
 }
