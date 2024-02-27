dfb7a212ed4d7d7dd040dc964b4da6570db4675a
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-02-27 14:32:24 +0900

    [level 1] Title: [1차] 다트 게임, Time: 1.57 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(String dartResult) {
+        Map<String, Integer> eventMap = new HashMap<>();
+        eventMap.put("S", 1);
+        eventMap.put("D", 2);
+        eventMap.put("T", 3);
+        eventMap.put("*", 2);
+        eventMap.put("#", -1);
+        int order = 0;
+        int[] dartScore = new int[3];
+
+        StringTokenizer st = new StringTokenizer(dartResult.replaceAll("[^0-9]", " "));
+        dartScore[0] = Integer.parseInt(st.nextToken());
+        dartScore[1] = Integer.parseInt(st.nextToken());
+        dartScore[2] = Integer.parseInt(st.nextToken());
+
+        for (String s : dartResult.split("")) {
+            if (s.matches("[0-9]")) {
+                continue;
+            }
+
+            if (eventMap.containsKey(s)) {
+                if (s.equals("S") || s.equals("D") || s.equals("T")) {
+                    order++;
+                    dartScore[order - 1] = (int) Math.pow(dartScore[order - 1], eventMap.get(s));
+                } else {
+                    if (s.equals("*")) {
+                        if (order >= 2) {
+                            dartScore[order - 2] = eventMap.get("*") * dartScore[order - 2];
+                        }
+                        dartScore[order - 1] = eventMap.get("*") * dartScore[order - 1];
+                    } else {
+                        dartScore[order - 1] = eventMap.get("#") * dartScore[order - 1];
+                    }
+                }
+            }
+        }
+        
+        return IntStream.of(dartScore).sum();
+    }
+}
