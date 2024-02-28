87d849d666dedd5d2d1a9400c08a4f4f3eb41590
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-02-28 16:18:44 +0900

    [level 1] Title: 신고 결과 받기, Time: 3.81 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        // 이메일 받은 횟수 저장할 Map 객체
+        Map<String, Integer> mailMap = new LinkedHashMap<>();
+        // 피고:신고인 저장할 Map 객체
+        Map<String, Set<String>> reportMap = new HashMap<>();
+
+        for (String id : id_list) {
+            mailMap.put(id, 0);
+        }
+        for (String rep : report) {
+            String defendant = rep.split(" ")[1];
+            String reporter = rep.split(" ")[0];
+
+            if (!reportMap.containsKey(defendant)) {
+                reportMap.put(defendant, new HashSet<>());
+            }
+            reportMap.get(defendant).add(reporter);
+        }
+
+        for (String defendant : reportMap.keySet()) {
+            if (reportMap.get(defendant).size() >= k) {
+                reportMap.get(defendant)
+                        .forEach(reporter -> mailMap.put(reporter, mailMap.getOrDefault(reporter, 0) + 1));
+            }
+        }
+        
+        return mailMap.values()
+                .stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
