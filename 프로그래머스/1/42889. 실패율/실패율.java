e7988181f7d73c29f900adae3e65deb7af5412f0
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-02-29 13:13:51 +0900

    [level 1] Title: 실패율, Time: 3.29 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        Arrays.sort(stages);
+        Queue<Integer> stageQueue = new LinkedList<>();
+        Map<Integer, Double> stageMap = new HashMap<>();
+
+        for (int stage : stages) {
+            stageQueue.add(stage);
+        }
+        for (int i = 1; i <= N; i++) {
+            int count = 0;
+            double size = stageQueue.size() == 0 ? 1.0 : stageQueue.size();
+
+            while (stageQueue.contains(i)) {
+                stageQueue.poll();
+                count++;
+            }
+            stageMap.put(i, count / size);
+        }
+        
+        List<Integer> stageList = new ArrayList<>(stageMap.keySet());
+        stageList.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (stageMap.get(o1).equals(stageMap.get(o2))) {
+                    return o1 - o2;
+                } else {
+                    return stageMap.get(o2).compareTo(stageMap.get(o1));
+                }
+            }
+        });
+        
+        return stageList.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
