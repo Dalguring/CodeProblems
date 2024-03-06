c0c429a13698589d4d20b9afccbc4a2f5f6760ef
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-03-06 09:08:23 +0900

    [level 1] Title: 소수 찾기, Time: 185.04 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    private static boolean isPrime(int i) {
+        for (int j = 2; j <= Math.sqrt(i); j++) {
+            if (i % j == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
