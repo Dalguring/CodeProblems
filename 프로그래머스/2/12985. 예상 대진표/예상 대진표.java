ac9ebb16b33294fab4dbd3e75c66843d787ae7fc
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-04-24 15:34:46 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,14 +2,15 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int count = 0;
-        
-        while (a != b) {
+        int count = 1;
+
+        while ((a + 1) / 2 != (b + 1) / 2) {
             a = makeEven(a) / 2;
             b = makeEven(b) / 2;
             count++;
         }
-
+        
+        // 서로 만나서 붙게 될 때도 라운드 계산을 하므로 + 1
         return count;
     }
     
