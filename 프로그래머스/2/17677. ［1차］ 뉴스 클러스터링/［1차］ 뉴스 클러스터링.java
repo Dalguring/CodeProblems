3ca77881a58869cb1fffacca58ba14102490ca88
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-05-20 11:36:48 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.01 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -6,8 +6,8 @@ class Solution {
         List<String> list1 = new ArrayList<>();
         List<String> list2 = new ArrayList<>();
 
-        list1 = makeMultiset(str1, list1);
-        list2 = makeMultiset(str2, list2);
+        makeMultiset(str1, list1);
+        makeMultiset(str2, list2);
 
         if (list1.size() == 0 && list2.size() == 0) {
             return 65536;
@@ -18,19 +18,17 @@ class Solution {
         int pointer = 0;
 
         while (pointer < list2.size()) {
-            if (list1.contains(list2.get(pointer))) {
-                list1.remove(list2.get(pointer));
-            }
+            list1.remove(list2.get(pointer));
             pointer++;
         }
-        
+
         intersection -= list1.size();
         union -= intersection;
         
         return (int) (intersection / (double) union * 65536);
     }
     
-    static List<String> makeMultiset(String str, List<String> list) {
+    static void makeMultiset(String str, List<String> list) {
         str = str.toLowerCase();
 
         for (int i = 0; i < str.length() - 1; i++) {
@@ -40,6 +38,5 @@ class Solution {
             }
         }
 
-        return list;
     }
 }
