ed464348a17dc02f82774c9b510c294d3995eff2
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-05-27 09:12:12 +0900

    [Bronze II] Title: 수 정렬하기, Time: 152 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,35 +1,62 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main {
+    private static int[] array;
+    private static int N;
+    private static BufferedReader br;
+    
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		
-		int N = Integer.parseInt(br.readLine());
-		List<Integer> list = new ArrayList<Integer>();
-		for(int i = 0; i < N; i++) {
-			list.add(Integer.parseInt(br.readLine()));
-		}
-		
-		Collections.sort(list);
-		for(int i : list) {
-			sb.append(i+"\n");
-		}
-		
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-		
-	}
+        array = new int[N];
+        bubbleSort();
 
+        for (int i : array) {
+            sb.append(i).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+
+    private static void getInput() throws IOException {
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(br.readLine());
+        }
+    }
+
+    private static void insertionSort() throws IOException {
+        getInput();
+
+        int exchange = 0;
+        for (int i = 1; i < array.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (array[i] < array[j]) {
+                    exchange = array[i];
+                    array[i] = array[j];
+                    array[j] = exchange;
+                }
+            }
+        }
+    }
+    
+    private static void bubbleSort() throws IOException {
+        getInput();
+        
+        int exchange = 0;
+        for (int i = 1; i < array.length; i++) {
+            for (int j = 0; j < array.length - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    exchange = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = exchange;
+                }
+            }
+        }
+    }
 }
