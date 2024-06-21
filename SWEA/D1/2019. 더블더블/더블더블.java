c4cf384968186869b7c2a3b994fd958f8d67613d
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-06-21 15:49:20 +0900

    [D1] Title: 더블더블, Time: 101 ms, Memory: 19,692 KB -BaekjoonHub

---
+++ SWEA/D1/2019. 더블더블/더블더블.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        br.close();
+
+        for (int i = 0; i <= input; i++) {
+            sb.append(1 << i).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
