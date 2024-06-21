a3e13a31a9b5c7cc2395bc7cb181310107eee1ef
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-06-21 14:11:27 +0900

    [D1] Title: 몫과 나머지 출력하기, Time: 110 ms, Memory: 24,112 KB -BaekjoonHub

---
+++ SWEA/D1/2029. 몫과 나머지 출력하기/몫과 나머지 출력하기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append(String.format("#%d %d %d", i + 1, a / b, a % b)).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
