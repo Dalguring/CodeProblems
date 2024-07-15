e341f5c19e0ff6243db5174a5b5d78f1f5799481
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-07-15 13:09:51 +0900

    [level 2] Title: 방문 길이, Time: 22.41 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public int solution(String dirs) {
+        List<String> visitList = new ArrayList<>();
+        Map<Character, int[]> directionMap = new HashMap<>();
+        directionMap.put('U', new int[]{0, 1});
+        directionMap.put('R', new int[]{1, 0});
+        directionMap.put('D', new int[]{0, -1});
+        directionMap.put('L', new int[]{-1, 0});
+
+        int x = 5;
+        int y = 5;
+        int move = 0;
+
+        for (char dir : dirs.toCharArray()) {
+            int dx = directionMap.get(dir)[0];
+            int dy = directionMap.get(dir)[1];
+
+            if (x + dx < 0 || x + dx > 10 || y + dy < 0 || y + dy > 10) {
+                continue;
+            }
+
+            if (visitList.contains("" + x + y + (x + dx) + (y + dy))
+                    || visitList.contains("" + (x + dx) + (y + dy) + x + y)) {
+                x = x + dx;
+                y = y + dy;
+
+                continue;
+            }
+
+            visitList.add("" + x + y + (x + dx) + (y + dy));
+            visitList.add("" + (x + dx) + (y + dy) + x + y);
+
+            x += dx;
+            y += dy;
+
+            move++;
+        }
+        
+        return move;
+    }
+}
