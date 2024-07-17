4b261f2a9a9f2868bab49dd607ee175a5cd826ee
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-07-17 13:48:24 +0900

    [level 2] Title: 택배상자, Time: 161.36 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,45 +1,16 @@
-import java.util.*;
+import java.util.Stack;
 
 class Solution {
     public int solution(int[] order) {
-        Queue<Integer> mainBelt = new LinkedList<>();
-        Stack<Integer> subBelt = new Stack<>();
-        int pointer = 0;
+        Stack<Integer> stack = new Stack<>();
         int count = 0;
-
+        
         for (int i = 1; i <= order.length; i++) {
-            mainBelt.offer(i);
-        }
-
-        while (!mainBelt.isEmpty()) {
-            int target = order[pointer];
-
-            if (mainBelt.peek() != target) {
-                if (!subBelt.isEmpty()) {
-                    if (subBelt.peek() == target) {
-                        subBelt.pop();
-                        pointer++;
-                        count++;
-                    } else {
-                        subBelt.push(mainBelt.poll());
-                    }
-                } else {
-                    subBelt.push(mainBelt.poll());
-                }
-            } else {
-                mainBelt.poll();
-                pointer++;
+            stack.push(i);
+            
+            while (!stack.isEmpty() && stack.peek() == order[count]) {
+                stack.pop();
                 count++;
-            }
-        }
-
-        if (!subBelt.isEmpty()) {
-            if (subBelt.peek() == order[pointer]) {
-                while (!subBelt.isEmpty() && subBelt.peek() == order[pointer]) {
-                    subBelt.pop();
-                    pointer++;
-                    count++;
-                }
             }
         }
 
