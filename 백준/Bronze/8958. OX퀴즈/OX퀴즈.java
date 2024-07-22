0ec43276cf07fd5b5b7367fb19519c7d0aa3065e
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-07-22 13:14:28 +0900

    [Bronze II] Title: OX퀴즈, Time: 144 ms, Memory: 14804 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -1,28 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		String[] arr = new String[N];
-		int isO = 0;
-		int total = 0;
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = sc.next();
-			String[] array = arr[i].split("");
-			for(String j : array) {
-				if(j.equals("O")) {
-					isO++;
-					total += isO;
-				} else if(j.equals("X")){
-					isO=0;
-				}
-			}
-			System.out.println(total);
-			total = 0;
-			isO=0;
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int totalCase = Integer.parseInt(br.readLine());
+        Pattern pattern = Pattern.compile("O+");
+        Matcher matcher;
+        StringBuilder sb = new StringBuilder();
 
+        for (int i = 0; i < totalCase; i++) {
+            matcher = pattern.matcher(br.readLine());
+            int value = 0;
+
+            while (matcher.find()) {
+                int size = matcher.group().length();
+                value += size * (1 + size) / 2;
+            }
+
+            sb.append(value).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
 }
