0a1866cd190b1d23b5dd0b371386af8c79a04728
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-07-23 09:24:51 +0900

    [Bronze III] Title: 팩토리얼, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,16 +1,20 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int target = Integer.parseInt(br.readLine());
-        br.close();
-        System.out.println(factorial(target));
-    }
+        int N = Integer.parseInt(br.readLine());
+        long[] factorials = new long[13];
+        factorials[0] = 1;
 
-    static int factorial(int target) {
-        if(target <= 1)
-            return 1;
-        return target * factorial(target - 1);
+        for (int i = 1; i < factorials.length; i++) {
+            factorials[i] = factorials[i - 1] * i;
+        }
+
+        System.out.println(factorials[N]);
+        br.close();
     }
 }
