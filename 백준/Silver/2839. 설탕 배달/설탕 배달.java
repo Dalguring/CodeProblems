22bc49d5258cd46869167939fd83cd82f658b579
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-10-15 11:39:48 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,32 +1,36 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
+    static int[] dp = new int[5001];
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int N = Integer.parseInt(br.readLine());
-		int fCount = 0;
-		int tCount = 0;
-		
-		for(int i = 0; i < N; i++) {
-			if((N - 5*i) < 0) { break;}
-			if((N - 5*i)%3 == 0) {
-				fCount = i;
-				tCount = (N - 5*i)/3;
-			} 
-		}
-		if(fCount + tCount == 0) {
-			System.out.println(-1);
-		} else {
-			System.out.println(fCount + tCount);
-		}
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        dp[3] = 1;
+        dp[5] = 1;
+
+        for (int i = 5; i < dp.length; i++) {
+            if (dp[i - 3] == 0 && dp[i - 5] == 0) {
+                continue;
+            }
+
+            if (i % 5 == 0) {
+                dp[i] = dp[i - 5] + 1;
+            } else {
+                dp[i] = dp[i - 3] + 1;
+            }
+        }
+
+        int answer = dp[Integer.parseInt(br.readLine())];
+
+        if (answer == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(answer);
+        }
+        
+        br.close();
+    }
 }
