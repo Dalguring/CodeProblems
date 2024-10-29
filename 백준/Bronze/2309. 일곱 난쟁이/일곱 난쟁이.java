556335af247d8e6f4034123caf6da9e3b9cd3992
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-10-29 13:54:34 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,49 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main {
-    static boolean[] visited = new boolean[9];
-    static int[] dwarfTall = new int[9];
-    static List<Integer> dwarfs = new ArrayList<>();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] dwarfTall = new int[9];
+        int total = 0;
+        boolean isOver = false;
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < 9; i++) {
             dwarfTall[i] = Integer.parseInt(br.readLine());
+            total += dwarfTall[i];
         }
 
-        getTall(0, 0, 0);
+        for (int i = 0; i < 8; i++) {
+            if (isOver) {
+                break;
+            }
+            for (int j = i + 1; j < 9; j++) {
+                if (total - dwarfTall[i] - dwarfTall[j] == 100) {
+                    dwarfTall[i] = -1;
+                    dwarfTall[j] = -1;
+                    isOver = true;
+                }
+            }
+        }
+
+        Arrays.sort(dwarfTall);
+        for (int i = 2; i < 9; i++) {
+            sb.append(dwarfTall[i]).append("\n");
+        }
+
+        System.out.println(sb);
+
         br.close();
-    }
-
-    static void getTall(int start, int tall, int confirmed) {
-        if (confirmed == 7) {
-            if (tall == 100) {
-                for (int i = 0; i < 9; i++) {
-                    if (visited[i]) dwarfs.add(dwarfTall[i]);
-                }
-
-                Collections.sort(dwarfs);
-                for (int height : dwarfs) {
-                    System.out.println(height);
-                }
-
-                System.exit(0);
-            }
-            return;
-        }
-
-        for (int i = start; i < 9; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                getTall(i + 1, tall + dwarfTall[i], confirmed + 1);
-                visited[i] = false;
-            }
-        }
     }
 }
