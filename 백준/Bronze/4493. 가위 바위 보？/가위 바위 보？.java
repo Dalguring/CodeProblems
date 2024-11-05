7e9c3862a3eaefe60ad4ef9013edba66b0bcdff8
Daljee <108657661+Dalguring@users.noreply.github.com>
2024-11-05 13:39:58 +0900

    [Bronze III] Title: 가위 바위 보?, Time: 220 ms, Memory: 27188 KB -BaekjoonHub

--- 백준/Bronze/4493. 가위 바위 보？/가위 바위 보？.java
+++ 백준/Bronze/4493. 가위 바위 보？/가위 바위 보？.java
@@ -1,45 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st;
+        int testCases = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            int t = Integer.parseInt(br.readLine());
-            int define = 0;
+        Map<String, String> winMap = new HashMap<>();
+        winMap.put("R", "S");
+        winMap.put("P", "R");
+        winMap.put("S", "P");
 
-            for (int j = 0; j < t; j++) {
-                st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < testCases; i++) {
+            int rounds = Integer.parseInt(br.readLine());
+            int player1Wins = 0;
+            int player2Wins = 0;
+
+            for (int j = 0; j < rounds; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
                 String p1 = st.nextToken();
                 String p2 = st.nextToken();
 
-                if (p1.equals("R") && p2.equals("P")) {
-                    define--;
-                } else if (p1.equals("R") && p2.equals("S")) {
-                    define++;
-                } else if (p1.equals("P") && p2.equals("R")) {
-                    define++;
-                } else if (p1.equals("P") && p2.equals("S")) {
-                    define--;
-                } else if (p1.equals("S") && p2.equals("R")) {
-                    define--;
-                } else if (p1.equals("S") && p2.equals("P")) {
-                    define++;
+                if (p1.equals(p2)) {
+                    continue;
+                }
+
+                if (winMap.get(p1).equals(p2)) {
+                    player1Wins++;
+                } else {
+                    player2Wins++;
                 }
             }
 
-            if (define < 0) {
-                sb.append("Player 2").append("\n");
-            } else if (define > 0) {
-                sb.append("Player 1").append("\n");
+            if (player1Wins > player2Wins) {
+                sb.append("Player 1\n");
+            } else if (player2Wins > player1Wins) {
+                sb.append("Player 2\n");
             } else {
-                sb.append("TIE").append("\n");
+                sb.append("TIE\n");
             }
         }
 
