966e76be6fb7c1367ca7c447cfaa661b9aa0ff28
Daljee <108657661+Dalguring@users.noreply.github.com>
2025-02-10 11:48:35 +0900

    [D2] Title: 조교의 성적 매기기, Time: 91 ms, Memory: 27,776 KB -BaekjoonHub

--- SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.java
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.java
@@ -4,64 +4,58 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Solution {
+    static class Student implements Comparable<Student> {
+        int id;
+        double score;
+
+        public Student(int id, double score) {
+            this.id = id;
+            this.score = score;
+        }
+
+        @Override
+        public int compareTo(Student o) {
+            return Double.compare(o.score, this.score);
+        }
+    }
+
+    private static final String[] GRADES = {
+        "A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"
+    };
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         int testcase = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < testcase; i++) {
-            st = new StringTokenizer(br.readLine());
-            int students = Integer.parseInt(st.nextToken());
-            int target = Integer.parseInt(st.nextToken());
-            int duplicationScore = students / 10;
-            Map<Integer, Double> studentScoreMap = new HashMap<>();
+        for (int tc = 0; tc < testcase; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+            int studentsPerGrade = N / 10;
 
-            for (int j = 1; j <= students; j++) {
-                String[] scores = br.readLine().split(" ");
-                double finalScore = Double.parseDouble(scores[0]) * 0.35
-                        + Double.parseDouble(scores[1]) * 0.45
-                        + Double.parseDouble(scores[2]) * 0.2;
+            Student[] students = new Student[N];
 
-                studentScoreMap.put(j, finalScore);
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                double midterm = Double.parseDouble(st.nextToken()) * 0.35;
+                double finals = Double.parseDouble(st.nextToken()) * 0.45;
+                double assignment = Double.parseDouble(st.nextToken()) * 0.2;
+                students[i] = new Student(i + 1, midterm + finals + assignment);
             }
 
-            List<Integer> keySet = new ArrayList<>(studentScoreMap.keySet());
-            keySet.sort(new Comparator<Integer>() {
-                @Override
-                public int compare(Integer o1, Integer o2) {
-                    return studentScoreMap.get(o2).compareTo(studentScoreMap.get(o1));
+            Arrays.sort(students);
+
+            int studentRank = 0;
+            for (int i = 0; i < N; i++) {
+                if (students[i].id == K) {
+                    studentRank = i;
+                    break;
                 }
-            });
-
-            int index = (int) Math.ceil(((double) keySet.indexOf(target) + 1) / duplicationScore);
-            String grade = "";
-
-            switch (index) {
-                case 1:
-                    grade = "A+"; break;
-                case 2:
-                    grade = "A0"; break;
-                case 3:
-                    grade = "A-"; break;
-                case 4:
-                    grade = "B+"; break;
-                case 5:
-                    grade = "B0"; break;
-                case 6:
-                    grade = "B-"; break;
-                case 7:
-                    grade = "C+"; break;
-                case 8:
-                    grade = "C0"; break;
-                case 9:
-                    grade = "C-"; break;
-                case 10:
-                    grade = "D0"; break;
             }
 
-            sb.append(String.format("#%d %s", i + 1, grade)).append("\n");
+            int gradeIndex = studentRank / studentsPerGrade;
+            sb.append(String.format("#%d %s\n", tc + 1, GRADES[gradeIndex]));
         }
 
         System.out.println(sb);
