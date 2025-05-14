6d132b2a2942af62e77f8702470d6f827993f2d9
Daljee <108657661+Dalguring@users.noreply.github.com>
2025-05-14 14:58:56 +0900

    [Bronze II] Title: 우유 축제, Time: 108 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Bronze/14720. 우유 축제/우유 축제.java
+++ 백준/Bronze/14720. 우유 축제/우유 축제.java
@@ -8,18 +8,45 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         br.readLine();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int memory = 0;
+        Milk younghak = new Milk();
         int count = 0;
 
         while (st.hasMoreTokens()) {
-            if (memory == Integer.parseInt(st.nextToken())) {
-                count += 1;
-                memory++;
-                memory %= 3;
+            int store = Integer.parseInt(st.nextToken());
+
+            if (younghak.isToDrink(store)) {
+                younghak.setNextDrink();
+                count++;
             }
         }
 
         System.out.println(count);
         br.close();
+    }
+}
+
+class Milk {
+    private int toDrink = 0;
+
+    public boolean isToDrink(int store) {
+        return store == toDrink;
+    }
+
+    public void setNextDrink() {
+        toDrink++;
+        toDrink %= 3;
+    }
+
+    public String getToDrink() {
+        return DrinkType.values()[toDrink].toString();
+    }
+
+    enum DrinkType {
+        STRAWBERRY, CHOCOLATE, BANANA;
+
+        @Override
+        public String toString() {
+            return name();
+        }
     }
 }
