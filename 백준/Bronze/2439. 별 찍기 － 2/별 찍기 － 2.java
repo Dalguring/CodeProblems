2b6337a94cc6c8977fbbecae5607084881b70db7
Daljee <108657661+Dalguring@users.noreply.github.com>
2025-05-22 14:05:51 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,17 +1,25 @@
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        for(int i = 1; i <= N; i++) {
-            for(int j = N-i; j >= 1; j--) {
-                System.out.print(" ");
-            }
-            for(int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int layer = Integer.parseInt(br.readLine());
+
+        dfs(1, layer);
+        System.out.print(sb);
+        br.close();
+    }
+
+    static void dfs(int depth, int layer) {
+        if (depth > layer) {
+            return;
         }
-        
+
+        sb.append(" ".repeat(layer - depth)).append("*".repeat(depth)).append("\n");
+        dfs(depth + 1, layer);
     }
 }
