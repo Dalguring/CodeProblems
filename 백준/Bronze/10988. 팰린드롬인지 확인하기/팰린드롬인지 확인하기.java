e6d32acddbbcf2d29ca2ae9d7310318d3571e5ec
Daljee <108657661+Dalguring@users.noreply.github.com>
2025-05-23 10:51:19 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -3,15 +3,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int pointer1 = 0;
+        int pointer2 = input.length() - 1;
+        boolean isPalindrom = true;
 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String oriStr = br.readLine();
-		br.close();
-		StringBuilder sb = new StringBuilder(oriStr);
-		if(oriStr.equals(sb.reverse().toString())) System.out.println(1);
-		else System.out.println(0);
-	}
+        while (pointer1 <= pointer2) {
+            if (input.charAt(pointer1) != input.charAt(pointer2)) {
+                isPalindrom = false;
+                break;
+            }
+            pointer1++;
+            pointer2--;
+        }
 
+        System.out.println(isPalindrom ? 1 : 0);
+        br.close();
+    }
 }
