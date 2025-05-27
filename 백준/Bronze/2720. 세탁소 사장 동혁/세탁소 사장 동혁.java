cdaa1aa15c81b2001034e4fb9fa10c41958f5e4e
Daljee <108657661+Dalguring@users.noreply.github.com>
2025-05-27 10:53:35 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -1,24 +1,31 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+    static int change;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testCase = Integer.parseInt(br.readLine());
-        int money = 0;
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for(int i = 0; i < testCase; i++) {
-            money = Integer.parseInt(br.readLine());
-            bw.append(money / 25 + " ");
-            money = money % 25;
-            bw.append(money / 10 + " ");
-            money = money % 10;
-            bw.append(money / 5 + " ");
-            money = money % 5;
-            bw.append(money+ "");
-            bw.flush();
-            System.out.println();
+        StringBuilder sb = new StringBuilder();
+        int testcase = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < testcase; i++) {
+            change = Integer.parseInt(br.readLine());
+            sb.append(getShare(25)).append(" ");
+            sb.append(getShare(10)).append(" ");
+            sb.append(getShare(5)).append(" ");
+            sb.append(getShare(1)).append("\n");
         }
+
+        System.out.print(sb);
         br.close();
-        bw.close();
+    }
+
+    static int getShare(int divider) {
+        int share = change / divider;
+        change %= divider;
+
+        return share;
     }
 }
