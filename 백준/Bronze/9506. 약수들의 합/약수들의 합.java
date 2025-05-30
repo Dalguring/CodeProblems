813ce64d43c6daec7054e270fc7f01b909802e54
Daljee <108657661+Dalguring@users.noreply.github.com>
2025-05-30 11:23:14 +0900

    [Bronze I] Title: 약수들의 합, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.java
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -1,44 +1,56 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
-    private final static Set<Integer> set = new TreeSet<>();
-    private final static StringBuilder sb = new StringBuilder();
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int target = 0;
+        String input;
+        StringBuilder sb = new StringBuilder();
 
-        while ((target = Integer.parseInt(br.readLine())) != -1) {
-            set.add(1);
-            addDivisor(target);
-            boolean equal = set.stream().mapToInt(Integer::intValue).sum() == target;
-            System.out.println(makeString(equal, target));
-            set.clear();
-        }
-        br.close();
-    }
+        while (!(input = br.readLine()).equals("-1")) {
+            int target = Integer.parseInt(input);
+            sb.append(target);
+            List<Integer> list = getDivisorSum(target);
+            Collections.sort(list);
+            
+            int sum = list.stream()
+                    .mapToInt(Integer::intValue)
+                    .sum();
 
-    static void addDivisor(int target) {
-        for (int i = 2; i <= Math.sqrt(target); i++) {
-            if (target % i == 0) {
-                set.add(i);
-                set.add(target / i);
+            if (target == sum) {
+                sb.append(" = ");
+
+                for (int i : list) {
+                    sb.append(i).append(" + ");
+                }
+                sb.delete(sb.length() - 3, sb.length());
+                sb.append("\n");
+            } else {
+                sb.append(" is NOT perfect.\n");
             }
         }
+
+        System.out.print(sb);
     }
 
-    static String makeString(boolean equal, int target) {
-        sb.setLength(0);
-        
-        if (equal) {
-            sb.append(target).append(" = ");
-            set.stream().forEach(elem -> sb.append(elem).append(" + "));
-            sb.delete(sb.length() - 3, sb.length());
-        } else {
-            sb.append(target).append(" is NOT perfect.");
+    static List<Integer> getDivisorSum(int number) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                list.add(i);
+
+                if (number / i != i) {
+                    list.add(number / i);
+                }
+            }
         }
 
-        return sb.toString();
+        return list;
     }
 }
