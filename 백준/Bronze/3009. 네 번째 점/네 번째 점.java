6552cad8236092af3bd6f57d1a18ab6472b5627a
Daljee <108657661+Dalguring@users.noreply.github.com>
2025-06-02 15:22:00 +0900

    [Bronze III] Title: 네 번째 점, Time: 108 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.java
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -1,30 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
-        List<Integer> xList = new ArrayList<>();
-        List<Integer> yList = new ArrayList<>();
-        for(int i = 0; i < 3; i++) {
-            st = new StringTokenizer(br.readLine());
-            xList.add(Integer.parseInt(st.nextToken()));
-            yList.add(Integer.parseInt(st.nextToken()));
-        }
-        Collections.sort(xList);
-        Collections.sort(yList);
-        StringBuilder sb = new StringBuilder();
-        if(xList.get(0).equals(xList.get(1))) sb.append(xList.get(2) + " ");
-        else sb.append(xList.get(0) + " ");
-        if(yList.get(0).equals(yList.get(1))) sb.append(yList.get(2));
-        else sb.append(yList.get(0));
+        StringTokenizer st;
+        Map<Integer, Integer> xMap = new HashMap<>();
+        Map<Integer, Integer> yMap = new HashMap<>();
+        int pointX = 0;
+        int pointY = 0;
 
-        System.out.println(sb);
+        for (int i = 0; i < 3; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            xMap.put(x, xMap.getOrDefault(x, 0) + 1);
+            yMap.put(y, yMap.getOrDefault(y, 0) + 1);
+        }
+
+        for (int key : xMap.keySet()) {
+            if (xMap.get(key) == 1) {
+                pointX = key;
+            }
+        }
+        for (int key : yMap.keySet()) {
+            if (yMap.get(key) == 1) {
+                pointY = key;
+            }
+        }
+
+        System.out.printf("%d %d", pointX, pointY);
+        br.close();
     }
 }
