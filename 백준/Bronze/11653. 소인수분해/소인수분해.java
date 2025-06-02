7f55c5a9e5415330057055a35de82ee4faca6902
Daljee <108657661+Dalguring@users.noreply.github.com>
2025-06-02 15:11:06 +0900

    [Bronze I] Title: 소인수분해, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -3,26 +3,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		if(n != 1) div(n);
-	}
-	
-	public static void div(int n) {
-		int num = 0;
-		if(n == 1) return;
-		
-		for(int i = 2; i <= n; i++) {
-			if(n % i == 0) {
-				System.out.println(i);
-				num = i;
-				break;
-			}
-		}
-		div(n / num);
-	}
-	
+        for (int i = 2; i <= Math.sqrt(input); i++) {
+            while (input % i == 0) {
+                sb.append(i).append("\n");
+                input /= i;
+            }
+        }
 
+        if (input != 1) {
+            sb.append(input);
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
 }
