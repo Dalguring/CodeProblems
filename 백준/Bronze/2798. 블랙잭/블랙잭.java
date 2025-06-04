2f1fcbe452a5cde4f277fb072d024425b1e0e510
Daljee <108657661+Dalguring@users.noreply.github.com>
2025-06-04 17:02:05 +0900

    [Bronze II] Title: 블랙잭, Time: 108 ms, Memory: 14580 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,37 +1,59 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int[] card;
+    static int standard;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        List<Integer> cardList = new ArrayList<>();
+        int cards = Integer.parseInt(st.nextToken());
+        standard = Integer.parseInt(st.nextToken());
+        card = new int[cards];
         st = new StringTokenizer(br.readLine());
-        int answer = 0;
 
-        while (st.hasMoreTokens())
-            cardList.add(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < cards; i++) {
+            card[i] = Integer.parseInt(st.nextToken());
+        }
 
-        br.close();
+        Arrays.sort(card);
+        int margin = getMarginalIndex(0,cards - 1);
+        int max = 0;
 
-        OUTER:
-        for (int i = 0; i < N - 2; i++) {
-            for (int j = i + 1; j < N - 1; j++) {
-                for (int k = j + 1; k < N; k++) {
-                    int number = cardList.get(i) + cardList.get(j) + cardList.get(k);
-                    if (number == M) {
-                        answer = M;
-                        break OUTER;
-                    }
-                    if (number < M) {
-                        answer = Math.max(answer, number);
+        for (int i = 0; i <= margin - 2; i++) {
+            for (int j = i + 1; j <= margin - 1; j++) {
+                for (int k = j + 1; k <= margin; k++) {
+                    int sum = card[i] + card[j] + card[k];
+
+                    if (sum <= standard) {
+                        max = Math.max(max, sum);
                     }
                 }
             }
         }
 
-        System.out.println(answer);
+        System.out.println(max);
+        br.close();
+    }
+
+    private static int getMarginalIndex(int left, int right) {
+        int result = -1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (card[mid] <= standard) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result == -1 ? 0 : result;
     }
 }
