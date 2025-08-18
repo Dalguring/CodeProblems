1241f451002f1ff533f82c44c2ebc2a6d3d946f1
Daljee <108657661+Dalguring@users.noreply.github.com>
2025-08-18 17:01:55 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,37 +1,37 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
+    static boolean[] visited;
 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int N = Integer.parseInt(br.readLine());
-		int cnt = 0;
-		OUTER:
-		for(int i = 0; i < N; i++) {
-			boolean[] check = new boolean[26];
-			String target = br.readLine();
-			int prev = 0;
-			for(int j = 0; j < target.length(); j++) {
-				if(prev != target.charAt(j)) {
-					if(check[target.charAt(j) - 'a'] == false) {
-						check[target.charAt(j) - 'a'] = true;
-						prev = target.charAt(j);
-					}
-					else {continue OUTER;}
-				}
-			}
-			cnt++;
-		}
-		
-		System.out.println(cnt);
-		
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testcase = Integer.parseInt(br.readLine());
+        int count = 0;
 
+        for (int i = 0; i < testcase; i++) {
+            visited = new boolean['z' - 'a' + 1];
+            char[] chars = br.readLine().toCharArray();
+            count += isGroupWord(chars) ? 1 : 0;
+        }
+
+        System.out.print(count);
+        br.close();
+    }
+
+    static boolean isGroupWord(char[] chars) {
+        visited[(char) (chars[0] - 'a')] = true;
+
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] != chars[i - 1]) {
+                if (visited[(char) (chars[i] - 'a')]) {
+                    return false;
+                }
+                visited[(char) (chars[i] - 'a')] = true;
+            }
+        }
+
+        return true;
+    }
 }
